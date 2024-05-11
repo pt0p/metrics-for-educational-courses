6feaed982e3c62d5d554fbabe1463a6a854fbcaf
sonyamat <sister.hamster@gmail.com>
2024-05-11 19:09:02 +0300

    remove unused example

--- report/app.py
+++ report/app.py
@@ -373,25 +373,5 @@ app.layout = html.Div([
     modules_reports
 ])
 
-# data = OrderedDict(
-#     [
-#         ("Date", ["2015-01-01", "2015-10-24", "2016-05-10", "2017-01-10", "2018-05-10", "2018-08-15"]),
-#         ("Region", ["Montreal", "Toronto", "New York City", "Miami", "San Francisco", "London"]),
-#         ("Temperature", [1, -20, 3.512, 4, 10423, -441.2]),
-#         ("Humidity", [10, 20, 30, 40, 50, 60]),
-#         ("Pressure", [2, 10924, 3912, -10, 3591.2, 15]),
-#     ]
-# )
-
-# df = pd.DataFrame(data)
-
-
-# app.layout = dash_table.DataTable(
-#     data=df.to_dict('records'),
-#     columns=[{'id': c, 'name': c} for c in df.columns],
-# )
-
-
-
 if __name__ == '__main__':
     app.run_server(debug=True)
